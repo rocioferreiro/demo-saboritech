e61fe47f16a92f88828fc443c9ba951c832a472c
rocioferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2021-10-13 15:18:42 -0300

    first commit

---
+++ src/main/java/com/example/demosaboritech/DemoSaboritechApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosaboritech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSaboritechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSaboritechApplication.class, args);
+	}
+
+}
